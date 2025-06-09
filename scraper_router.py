ec0ebe538a05d03146567c496e5c987601f967c6
ahmet1407 <59695777+ahmet1407@users.noreply.github.com>
2025-06-09 14:19:43 +0300

    Update scraper_router.py

--- scraper_router.py
+++ scraper_router.py
@@ -1,17 +1,7 @@
 from utils.detect_platform import detect_platform
-from utils.scrape_amazon import scrape_amazon_product
-from utils.scrape_hepsiburada import scrape_hepsiburada_product
-from utils.scrape_trendyol import scrape_trendyol_product
+from scraper_manager import get_scraper_for_url
 
-def scrape_link(link_or_query: str) -> dict:
+def scrape_link(link_or_query):
     platform = detect_platform(link_or_query)
-    print(f"🧭 Platform tespiti: {platform}")
-
-    if platform == "amazon":
-        return scrape_amazon_product(link_or_query)
-    elif platform == "hepsiburada":
-        return scrape_hepsiburada_product(link_or_query)
-    elif platform == "trendyol":
-        return scrape_trendyol_product(link_or_query)
-    else:
-        raise ValueError("Desteklenmeyen platform veya geçersiz link.")
+    scraper = get_scraper_for_url(platform)
+    return scraper(link_or_query)
